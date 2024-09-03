17d432c6f97a52939979508b567533adacdf8a62
Jmafrath001 <jainulafrath05@gmail.com>
2024-09-03 17:36:38 +0530

    Day1 task

---
+++ Task1.js
@@ -0,0 +1,29 @@
+TASK 1
+
+Memberful: No links to intercept, quitting.
+alert('Afrath');
+document.body.innerHTML='Afrath';
+'Afrath'
+alert('Good Morning');
+undefined
+alert('afrath');
+undefined
+10+5
+15
+20-5
+15
+2+2-5
+-1
+document.body.innerHTML=('Good Morning');
+'Good Morning'
+documen.body.innerHTML=('Afrath');
+VM700:1 Uncaught ReferenceError: documen is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM700:1Understand this error
+document.body.innerHTML=('Afrath');
+'Afrath'
+10+8+20
+38
+100-20-50+200
+230
+document.body.innerHTML=(' ');
---
+++ Task3.js
@@ -0,0 +1,32 @@
+ 'My name is : ' 
+'My name is : '
+'Afrath'
+'Afrath'
+'My name is : '+'Afrath'
+'My name is : Afrath'
+`Total cost :$${5+3}`
+'Total cost :$8'
+'Total cost :$'+(5+3)
+'Total cost :$8'
+alert();
+undefined
+alert(`Total cost :$${5+3}`);
+undefined
+`Total cost: $${(599+295)/100}`
+'Total cost: $8.94'
+'Total cost: $'+((599+295)/100)
+'Total cost: $8.94'
+alert(`Total cost: $${(599+295)/100}`);
+undefined
+'Total cost: $8.94'
+alert(`Total cost: $${(599+295)/100}
+Thank you,Come again`);  
+
+`Items(${2+2}): $${(2095*2+799*2)/100}`
+'Items(4): $57.88'
+`Shipping & handling: $${(499+499)/100}`
+'Shipping & handling: $9.98'
+`Total before tax: $${(2095*2+799*2)/100+(499+499)/100}`
+'Total before tax: $67.86'
+'Estimated tax(10%):  $' + Math.round(6786*0.1)/100
+'Estimated tax(10%):  $6.79'                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
